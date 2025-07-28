8d87f8246f069b0281a0c6edd1f9825a182bba7b
Juanma <veronjuan111@gmail.com>
2025-07-28 18:34:56 -0300

    Integración del abm_evento y otras modificaciones.

--- proyecto_integrador/src/main/java/com/app_eventos/App.java
+++ proyecto_integrador/src/main/java/com/app_eventos/App.java
@@ -1,13 +1,26 @@
 package com.app_eventos;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Confirmar ruta
+        System.out.println(getClass().getResource("/fxml/abm/abmEventoResources/abm_evento.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/abm/abmEventoResources/abm_evento.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("ABM Evento");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
+
