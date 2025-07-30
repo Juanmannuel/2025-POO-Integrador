31cdd5f72e73fdd8c3a60352939a74f44cad96b3
Juanma <veronjuan111@gmail.com>
2025-07-30 17:38:47 -0300

    Modificación de la rama del proyecto e implementación de la interfaz de la App y el abmEvento.

--- proyecto_integrador/src/main/java/com/app_eventos/App.java
+++ proyecto_integrador/src/main/java/com/app_eventos/App.java
@@ -7,20 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        // Confirmar ruta
-        System.out.println(getClass().getResource("/fxml/abm/abmEventoResources/abm_evento.fxml"));
+@Override
+public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+    Scene scene = new Scene(root);
+    primaryStage.setTitle("Pantalla Principal de Eventos");
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/abm/abmEventoResources/abm_evento.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("ABM Evento");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+    // Tamaño inicial grande (ej: 1200x800)
+    primaryStage.setWidth(1200);
+    primaryStage.setHeight(800);
+
+    // Permitir maximizar correctamente
+    primaryStage.setMinWidth(800);
+    primaryStage.setMinHeight(600);
+
+    primaryStage.setScene(scene);
+    primaryStage.show();
+}
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
