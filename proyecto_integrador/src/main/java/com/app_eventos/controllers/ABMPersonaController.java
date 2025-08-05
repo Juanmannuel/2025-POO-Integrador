9c75cb3b2766f8f5f2bc1e8b4cd61b0b1d06f1c6
Juanma <veronjuan111@gmail.com>
2025-08-05 19:36:11 -0300

    Estilos incorporados a todas las pantallas, creación de los archivos para la pantalla inicio.

--- proyecto_integrador/src/main/java/com/app_eventos/controllers/ABMPersonaController.java
+++ proyecto_integrador/src/main/java/com/app_eventos/controllers/ABMPersonaController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.StackPane;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 public class ABMPersonaController {
 
